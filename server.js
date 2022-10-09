b85a6d61b2fd6100e574dc7c178b3be6165f4d6b
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-10-08 18:10:17 -0700

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { totalmem } = require("os")
 
 
 const app = express()
@@ -12,7 +13,12 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`Hello ${req.params.name}`)
 })
 
-
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    let total = parseInt(req.params.total)
+    let tipPercentage = parseInt(req.params.tipPercentage)
+    const tip = total * (tipPercentage/100)
+    res.send(`if the Bill is $${total} and the percentage you'd like to tip is ${tipPercentage}% than you should leave $${tip}`)
+})
 
 
 
