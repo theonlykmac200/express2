10a6413ceb5521598b75908b2d6f1724f28c3525
Kelly McDonald <kellymcdonald@Admins-MacBook-Air.local>
2022-10-15 19:22:38 -0700

    finshing first part of Express 2 with Magic8ball and the html tags.

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     const tip = total * (tipPercentage/100)
     res.send(`if the Bill is $${total} and the percentage you'd like to tip is ${tipPercentage}% than you should leave $${tip}`)
 })
+const magicAnswer = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
+app.get("/magic/:thisIsTheQuestion", (req, res) => {
+    res.send(`<div><h1>The answer to the question: "${(req.params.thisIsTheQuestion)}?" is:</h1><h1> ${magicAnswer[Math.floor(Math.random()*magicAnswer.length)]}</h1></div>`)
+
+})
 
 
 
