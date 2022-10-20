37428b2311dbad56dc8294cd297b6482a754ea3d
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-10-19 20:37:37 -0600

    add feedback

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express")
+
+//sometimes you will be strange auto adds by vs code, this one isn't causing an error but sometimes they do, so keep an eye out for these
 const { totalmem } = require("os")
 
 
@@ -16,13 +18,17 @@ app.get("/greeting/:name", (req, res) => {
 app.get("/tip/:total/:tipPercentage", (req, res) => {
     let total = parseInt(req.params.total)
     let tipPercentage = parseInt(req.params.tipPercentage)
-    const tip = total * (tipPercentage/100)
+    const tip = total * (tipPercentage / 100)
+    //great full message, helps to see what is really happening! 
     res.send(`if the Bill is $${total} and the percentage you'd like to tip is ${tipPercentage}% than you should leave $${tip}`)
 })
 const magicAnswer = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
+//nice job using Math.random(), it might be easier to use/read if you make it a variable and pass the variable
 app.get("/magic/:thisIsTheQuestion", (req, res) => {
-    res.send(`<div><h1>The answer to the question: "${(req.params.thisIsTheQuestion)}?" is:</h1><h1> ${magicAnswer[Math.floor(Math.random()*magicAnswer.length)]}</h1></div>`)
+    //rando variable used to break up the code
+    const rando = Math.floor(Math.random() * magicAnswer.length)
+    res.send(`<div><h1>The answer to the question: "${(req.params.thisIsTheQuestion)}?" is:</h1><h1> ${magicAnswer[rando]}</h1></div>`)
 
 })
 
